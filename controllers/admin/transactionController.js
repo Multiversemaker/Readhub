235fee759b7ee42571d13f7a907ab2af167406ce
2172018_Mochammad Rizky Fahrezy <161426030+ezy123456@users.noreply.github.com>
2026-01-21 21:30:53 +0700

    Update transactionController.js

--- controllers/admin/transactionController.js
+++ controllers/admin/transactionController.js
@@ -1,11 +1,12 @@
-// Panggil file-file pecahan di folder transaction-management
 const { getTransactionAll } = require("./transaction/listTransactionController");
-const { getCreateTransaction } = require(".//transaction/formTransactionController");
-const { store, editTransaction } = require("./transaction/crudTransactionController");
+const { getCreateTransaction } = require("./transaction/formTransactionController");
+const { store, editTransaction, approveTransaction, updateDueDate } = require("./transaction/crudTransactionController");
 
 module.exports = {
     getTransactionAll,
     getCreateTransaction,
     store,
     editTransaction,
+    approveTransaction,
+    updateDueDate 
 };
